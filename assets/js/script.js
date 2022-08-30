b1470fb13efd0002f3c4d3abf54eeacd40c2cfbb
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-08-30 10:09:34 -0500

    Added load from local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Testing JQuery
-$("header").click(function() {
+$("header").click(function () {
     console.log("The title has been clicked.");
 });
 // Grab the current hour to be used in inputEl
@@ -11,12 +11,12 @@ function generateInputEl(hour) {
     console.log(hour);
     var inputEl = document.createElement("input");
     inputEl.setAttribute("type", "text");
-    // Use the id to call and save localStorage.
+    // Use the id to call localStorage.
     inputEl.id = hour;
-    inputEl.value = "This is where the text will go";
-    if(hour < currentHour) {
+    inputEl.value = localStorage.getItem(hour);
+    if (hour < currentHour) {
         inputEl.classList.add("past");
-    } else if (hour === currentHour) {
+    } else if (hour == currentHour) {
         inputEl.classList.add("present");
     } else {
         inputEl.classList.add("future");
@@ -32,25 +32,25 @@ function generateSaveBtnEl() {
     saveBtnEl.classList.add("saveBtn");
 
 
-    return saveBtnEl; 
+    return saveBtnEl;
 }
 
 // Generate rows
 function generateRows() {
     //currentHour = moment().format("hh");
-    for(let i = 9; i <= 17; i++) {
+    for (let i = 9; i <= 17; i++) {
         var row = document.createElement("div");
         row.setAttribute("class", "row time-block");
-        if(i < 12) {
+        if (i < 12) {
             row.innerHTML = `<div class="hour">${i}AM</div>`;
-        } else if(i === 12) {
+        } else if (i === 12) {
             row.innerHTML = `<div class="hour">${i}PM</div>`;
         } else {
             row.innerHTML = `<div class="hour" id="${i}">${i - 12}PM</div>`;
         }
         row.append(generateInputEl(i));
         row.append(generateSaveBtnEl());
-        
+
         $(".container").append(row);
         console.log("row appended");
     }
@@ -71,11 +71,21 @@ function saveToLocal() {
 // Retrieve Local Storage 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     var m = moment().format("dddd, MMMM D");
+    if (m == localStorage.getItem("m")) {
+        // had to set if statement up like this due to === and !== funky business
+        console.log("Dates match");
+    } else {
+        localStorage.clear();
+        console.log("Local storage has been cleared");
+    }
+    localStorage.setItem("m", m);
     console.log("The document is ready!");
     console.log(m);
+    console.log(localStorage.getItem("m"));
     $("#currentDay").text(m);
+    // If localStorage dates do not match, clear localStorage
     generateRows();
     $(".saveBtn").click(saveToLocal);
 });
